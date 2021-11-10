6f330609b6fb559116ed6de1e8b889d68f7799dc
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-10 15:09:43 +0100

    Initial commit

---
+++ src/hu/petrik/Set/Set.java
@@ -0,0 +1,30 @@
+package hu.petrik.Set;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Set<T> { // T típusu elemek fogadására lesz alkalmas
+
+    private List<T> elements;
+
+    public Set() {
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        if (!this.elements.contains(element)){
+            this.elements.add(element);
+        }
+
+    }
+
+    public boolean remove(T element) {
+        boolean isRemoved = false;
+
+        if (this.elements.contains(element)) {
+            this.elements.remove(element);
+        }
+
+        return isRemoved;
+    }
+}
