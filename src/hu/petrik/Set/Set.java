074248497778a36417cb5056549baf6d007a6dae
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-10 15:38:28 +0100

    size + clear + iselement + isempty + ispartof + isequals

--- src/hu/petrik/Set/Set.java
+++ src/hu/petrik/Set/Set.java
@@ -19,6 +19,8 @@ public class Set<T> { // T típusu elemek fogadására lesz alkalmas
     }
 
     public boolean remove(T element) {
+        //return this.elements.remove(element); -> ugyanazt csinálja, mint a lenti sorok
+
         boolean isRemoved = false;
 
         if (this.elements.contains(element)) {
@@ -27,5 +29,47 @@ public class Set<T> { // T típusu elemek fogadására lesz alkalmas
         }
 
         return isRemoved;
+    }
+
+    public int getSize() {
+        return this.elements.size();
+    }
+
+    public void clear() {
+        this.elements.clear();
+    }
+
+    public boolean isElement(T element) {
+        int i = 0;
+        //boolean isElement = false;
+
+        while (i < this.elements.size() && this.elements.get(i) != element) {
+            i++;
+
+        }
+
+        return i<this.elements.size();
+    }
+
+    public boolean isEmpty() {
+        return this.elements.isEmpty(); //this.elements.getSize() == 0;
+    }
+
+    public boolean isPartOf(Set<T> otherSet) {
+        boolean isBigger = this.getSize() <= otherSet.getSize();
+        boolean isPartOf = false;
+        int i = 0;
+
+        if (!isBigger) {
+            while (i < this.getSize() && otherSet.isElement(this.elements.get(i))) {
+                i++;
+            }
+            isPartOf = i == this.getSize();
+        }
+        return isPartOf;
+    }
+
+    public boolean isEquals (Set<T> otherSet) {
+        return this.getSize() == otherSet.getSize() && this.isPartOf(otherSet);
     }
 }
